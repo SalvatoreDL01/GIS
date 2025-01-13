21848d11452dc17d9858ebd0844f24ae698038b7
SalvatoreDL01 <104557779+SalvatoreDL01@users.noreply.github.com>
2025-01-13 18:20:30 +0100

    aggiunta funzioni mappa

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 import folium
+from folium.plugins import HeatMapWithTime
 from utils import get_mappaggio
 
 
@@ -11,42 +12,156 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Crea la mappa
-    map = folium.Map(location=[43.0, 12.0], zoom_start=6)
+    map_left1 = folium.Map(tiles=None, location=[43.0, 12.0], zoom_start=6)
+    folium.TileLayer("OpenStreetMap").add_to(map_left1)
+    map_left2 = folium.Map(tiles=None, location=[43.0, 12.0], zoom_start=6)
+    folium.TileLayer("OpenStreetMap").add_to(map_left2)
+    map_left3 = folium.Map(tiles=None, location=[43.0, 12.0], zoom_start=6)
+    folium.TileLayer("OpenStreetMap").add_to(map_left3)
+    map_left4 = folium.Map(tiles=None, location=[43.0, 12.0], zoom_start=6)
+    folium.TileLayer("OpenStreetMap").add_to(map_left4)
 
+    map_right1 = folium.Map(location=[42.0, 9.0], zoom_start=6)
+    folium.TileLayer("OpenStreetMap").add_to(map_right1)
+    map_right2 = folium.Map(location=[42.0, 9.0], zoom_start=6)
+    folium.TileLayer("OpenStreetMap").add_to(map_right2)
+    map_right3 = folium.Map(location=[42.0, 9.0], zoom_start=6)
+    folium.TileLayer("OpenStreetMap").add_to(map_right3)
+    map_right4 = folium.Map(location=[42.0, 9.0], zoom_start=6)
+    folium.TileLayer("OpenStreetMap").add_to(map_right4)
+    
+    #folium.LayerControl().add_to(map_left)
+   
     # Aggiungi lo shapefile alla mappa
-    map = mappaggio.add_shp_to_map('data/province/Province.geojson', map)
+    mappaggio.add_shp_to_map('data/confini/puglia/puglia.geojson', map_left1)
+    mappaggio.add_shp_to_map('data/confini/puglia/puglia.geojson', map_left2)
+    mappaggio.add_shp_to_map('data/confini/puglia/puglia.geojson', map_left3)
+    mappaggio.add_shp_to_map('data/confini/puglia/puglia.geojson', map_left4)
 
-    # Codice JavaScript personalizzato
-    custom_js = """
-    <script>
-        // Accedi alla mappa
-        document.addEventListener('DOMContentLoaded', function() {
-            var map = document.querySelector('.folium-map');
+    mappaggio.add_shp_to_map('data/confini/lombardia/lombardia.geojson', map_right1)
+    mappaggio.add_shp_to_map('data/confini/lombardia/lombardia.geojson', map_right2)
+    mappaggio.add_shp_to_map('data/confini/lombardia/lombardia.geojson', map_right3)
+    mappaggio.add_shp_to_map('data/confini/lombardia/lombardia.geojson', map_right4)
 
-            // Personalizza i layer
-            var layers = document.querySelectorAll('path'); // Seleziona i layer (ad esempio i confini delle province)
-            layers.forEach(function(layer) {
-                layer.addEventListener('click', function() {
-                    if(this.style.fill == 'green'){
-                        this.style.stroke = '';  // Ripristina il colore del bordo
-                        this.style.strokeWidth = '';
-                        this.style.fill = ''; // Ripristina il colore di riempimento
-                    }else{
-                        this.style.stroke = 'green';  // Cambia colore al bordo
-                        this.style.fill = 'green';
-                        this.style.strokeWidth = '3px';
-                    }
-                });
-            });
-        });
+    #Heatmap Puglia
+    puglia_data, time_index = mappaggio.get_data_HeatMap('data/gas/puglia/NO2.csv', 'valore_inquinante_misurato', 'data_di_misurazione', 'Latitude', 'Longitude')
+    HeatMapWithTime(puglia_data,index=time_index).add_to(map_left1)
+
+    puglia_data, time_index = mappaggio.get_data_HeatMap('data/gas/puglia/O3.csv', 'valore_inquinante_misurato', 'data_di_misurazione', 'Latitude', 'Longitude')
+    HeatMapWithTime(puglia_data,index=time_index).add_to(map_left2)
+
+    puglia_data, time_index = mappaggio.get_data_HeatMap('data/gas/puglia/PM10.csv', 'valore_inquinante_misurato', 'data_di_misurazione', 'Latitude', 'Longitude')
+    HeatMapWithTime(puglia_data,index=time_index).add_to(map_left3)
+
+    puglia_data, time_index = mappaggio.get_data_HeatMap('data/gas/puglia/PM2.5.csv', 'valore_inquinante_misurato', 'data_di_misurazione', 'Latitude', 'Longitude')
+    HeatMapWithTime(puglia_data,index=time_index).add_to(map_left4)
+    
+    #Heatmap Lombardia
+    lombardia_data, time_index = mappaggio.get_data_HeatMap('data/gas/lombardia/NO2.csv', 'valore_inquinante_misurato', 'data_di_misurazione', 'Latitude', 'Longitude')
+    HeatMapWithTime(lombardia_data,index=time_index).add_to(map_right1)
+
+    lombardia_data, time_index = mappaggio.get_data_HeatMap('data/gas/lombardia/O3.csv', 'valore_inquinante_misurato', 'data_di_misurazione', 'Latitude', 'Longitude')
+    HeatMapWithTime(lombardia_data,index=time_index).add_to(map_right2)
+
+    lombardia_data, time_index = mappaggio.get_data_HeatMap('data/gas/lombardia/PM10.csv', 'valore_inquinante_misurato', 'data_di_misurazione', 'Latitude', 'Longitude')
+    HeatMapWithTime(lombardia_data,index=time_index).add_to(map_right3)
+
+    lombardia_data, time_index = mappaggio.get_data_HeatMap('data/gas/lombardia/PM25.csv', 'valore_inquinante_misurato', 'data_di_misurazione', 'Latitude', 'Longitude')
+    HeatMapWithTime(lombardia_data,index=time_index).add_to(map_right4)
+
+
+    #folium.LayerControl().add_to(map_left)
+
+    map_left_NO2_html = map_left1._repr_html_()
+    map_left_O3_html = map_left2._repr_html_()
+    map_left_PM10_html = map_left3._repr_html_()
+    map_left_PM25_html = map_left4._repr_html_()
+
+    map_right_NO2_html = map_right1._repr_html_()
+    map_right_O3_html = map_right2._repr_html_()
+    map_right_PM10_html = map_right3._repr_html_()
+    map_right_PM25_html = map_right4._repr_html_()
+
+    html = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Analisi gas in Puglia e Lombardia</title>
+        <style>
+            .testo {{
+                font-family: Arial, Helvetica, sans-serif;
+            }}
+            #map-container {{
+                display: flex;
+            }}
+            .map {{
+                width: 50%;
+                height: 600px;
+            }}
+            .container {{
+                width: 100%;
+            }}
+            #map-container-o3 {{
+                display: flex;
+            }}
+            #map-container-no2, #map-container-pm10, #map-container-pm25 {{
+                display: none;
+            }}
+        </style>
+    </head>
+    <body>
+        <h2 class="testo">Analisi gas in Puglia e Lombardia</h2>
+        <label for="cars" class="testo">Gas da analizzare: </label>
+        <select name="cars" id="mySelect" class="testo" onchange="myFunction()">
+            <option value="map-container-no2">NO2</option>
+            <option value="map-container-o3">O3</option>
+            <option value="map-container-pm10">PM10</option>
+            <option value="map-container-pm25">PM2.5</option>
+        </select>
+        <br>
+        <div id="map-container">
+            <div class="container" id="map-container-no2">
+                <div class="map">{map_left_NO2_html}</div>
+                <div class="map">{map_right_NO2_html}</div>
+            </div>
+
+            <div class="container" id="map-container-o3">
+                <div class="map">{map_left_O3_html}</div>
+                <div class="map">{map_right_O3_html}</div>
+            </div>
+
+            <div class="container" id="map-container-pm10">
+                <div class="map">{map_left_PM10_html}</div>
+                <div class="map">{map_right_PM10_html}</div>
+            </div>
+
+            <div class="container" id="map-container-pm25">
+                <div class="map">{map_left_PM25_html}</div>
+                <div class="map">{map_right_PM25_html}</div>
+            </div>
+            
+        </div>
+
+        <script>
+        function myFunction() {{
+        // Recupera il valore selezionato
+            var selectedValue = document.getElementById("mySelect").value;
+
+            // Nasconde tutti i contenitori di mappe
+            var containers = document.querySelectorAll(".container");
+            containers.forEach(function(container) {{
+                container.style.display = "none";
+            }});
+
+            // Mostra il contenitore corrispondente al valore selezionato
+            document.getElementById(selectedValue).style.display = "flex";
+        }}
     </script>
+    </body>
+    </html>
     """
-    
-    # Inserisci il JavaScript nella mappa
-    map.get_root().html.add_child(folium.Element(custom_js))
-    
-    # Ritorna la mappa come HTML
-    return map._repr_html_()
+    #html.save("dual_map_different_centers.html")
+    return html
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,56 @@
 import pandas as pd
 import geopandas
 import folium
+from folium.plugins import HeatMapWithTime
+from folium.plugins import DualMap
+
+def normalize(group, colonna):
+        min_val = group[colonna].min()
+        max_val = group[colonna].max()
+        group[colonna] = (group[colonna] - min_val) / (max_val - min_val)
+        return group
 
 class mappaggio:
+
+    @staticmethod
+    def get_data_HeatMap(path, colonna_valore, colonna_data, latitudine, longitudine):
+        data = pd.read_csv(path)
+
+        data = data[[colonna_valore, colonna_data, latitudine, longitudine]]
+
+        normalized_data = normalize(data, colonna_valore)
+        normalized_data[colonna_data] = pd.to_datetime(normalized_data[colonna_data])
+        gruped_data = normalized_data.groupby(colonna_data)
+
+        heat_data = [
+        group[[latitudine, longitudine, colonna_valore]].values.tolist()
+        for _, group in gruped_data
+        ]
+        time_index = [date.strftime('%Y-%m-%d') for date, _ in normalized_data.groupby(colonna_data)]
+
+        return heat_data, time_index
+    
+    @staticmethod
+    def dual_Map():
+        # Creazione della mappa divisa
+        dual_map = DualMap(location=[41.9028, 12.4964], zoom_start=6)
+
+        # Aggiunta della prima mappa (sinistra)
+        folium.Marker(
+            location=[41.9028, 12.4964],
+            popup="Roma"
+        ).add_to(dual_map.m1)
+
+        # Aggiunta della seconda mappa (destra)
+        folium.Marker(
+            location=[45.4642, 9.1900],
+            popup="Milano"
+        ).add_to(dual_map.m2)
+
     @staticmethod
     def add_shp_to_map(path,map):
+
+        fg = folium.FeatureGroup(name="Puglia", control = False).add_to(map)
 
         province = geopandas.read_file(path)
         layer_province = folium.GeoJson(
@@ -14,23 +60,7 @@ class mappaggio:
             "fillColor": "#00BFFF",  # Colore iniziale di riempimento
             "weight": 1,
             "fillOpacity": 0.2,
-        },
-        highlight_function=lambda feature: {
-            "fillColor": (
-                "red"
-            ),
-            "color" : "red",
-            "weight" : 2,
-            "bringToFront": True
-        },
-        tooltip=folium.features.GeoJsonTooltip(
-            fields=["NOME_PRO"],  # Sostituisci "nome" con il campo del GeoJSON contenente i nomi delle province
-            aliases=["Provincia:"],  # Alias per il campo (opzionale)
-            sticky=True  # Tooltip sempre visibile
-        ),
-        ).add_to(map)
+        }
+        ).add_to(fg)
 
         map.fit_bounds(layer_province.get_bounds())
-
-        return map
-
